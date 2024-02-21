4f06ae6a28bd854993399a0c2e0ddc02f8d37b61
AndersoonMendes <160607469+AndersoonMendes@users.noreply.github.com>
2024-02-21 14:23:53 +0000

    Lista de Personagem

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ app.get('/', function (req, res) {
 })
 
 app.get('/oi', function(req, res){
-   res.send('Olá mundo')
+   res.send('Olá Mundo')
+})
+
+//Lista de Personagens
+const lista = ['Saitama', 'Genos', 'King']
+
+//read all -> (get)/item
+app.get('/item', function(req, res){
+  res.send(lista)
 })
 
 app.listen(3000)
