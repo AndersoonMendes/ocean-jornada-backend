fa832e8298e13e44eef18b5aeb4432286b019681
AndersoonMendes <160607469+AndersoonMendes@users.noreply.github.com>
2024-02-21 15:06:37 +0000

    Lista: criar, ler todos, ler individual

--- index.js
+++ index.js
@@ -10,11 +10,39 @@ app.get('/oi', function(req, res){
 })
 
 //Lista de Personagens
-const lista = ['Saitama', 'Genos', 'King']
-
+const lista = ['Saitama', 'Genos', 'King', 'Cavaliro sem Licença', 'Tornado', 'Silver Fang', 'Metal Bat']
+//   ids:             0     1         2               3                 4          5               6   
 //read all -> (get)/item
 app.get('/item', function(req, res){
   res.send(lista)
+})
+
+//read by id -> (get)/item/:id
+app.get('/item/:id', function(req, res){
+  const id = req.params.id
+
+  //acessa o item na lista baseaddo no id recebido
+  const item = lista[id]
+
+  //envio do item obtido atrasves do id na lista
+  res.send(item)
+})
+
+//sinalizando que o body esta em json
+app.use(express.json())
+
+//create -> (post)/item
+app.post('/item', function(req, res){
+  //extraindo o corpo da requisição
+  const body = req.body
+
+  //pegamos a string(nome) que foi enviado no body
+  const item = body.nome
+
+  //adiciona um nome dentro da lista
+  lista.push(item)
+
+  res.send('item adicionado com sucesso')
 })
 
 app.listen(3000)
